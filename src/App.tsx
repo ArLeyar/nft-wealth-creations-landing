77bf7b3e3168305337e32e68625745580b1d8383
Ar Leyar <arleyar@yandex.com>
2025-04-27 11:11:21 +0800

    used pitch deck as index

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PitchDeck from "./pages/PitchDeck";
 import NotFound from "./pages/NotFound";
 
@@ -16,7 +15,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navigate to="/pitch-deck" replace />} />
+          <Route path="/" element={<PitchDeck />} />
           <Route path="/pitch-deck" element={<PitchDeck />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
